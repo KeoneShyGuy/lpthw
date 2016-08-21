23fb496508c08dc5945d0e3293d68471e16b69fa
KeoneShyGuy <KeoneShyGuy@users.noreply.github.com>
2016-08-21 05:14:22 -0400

    Add files via upload

--- ex14.py
+++ ex14.py
@@ -9,4 +9,13 @@ print "do you like me %s?" % user_name
 likes = raw_input(prompt)
 
 print "Where do you live %s?" % user_name
-lives = raw_input
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+""" % (likes, lives, computer)
